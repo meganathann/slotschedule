2b98292cc4f59a1cf78b2dc5e40e1903b23505e1
Meganathan N <meganathanmaggi25@gmail.com>
2024-02-09 15:47:54 +0530

    color change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ const Login = ({ onLogin }) => {
       <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
         <LockOutlinedIcon />
       </Avatar>
-      <Typography component="h1" variant="h5">
+      <Typography component="h1" variant="h5" color={"black"}>
         Sign in
       </Typography>
 
